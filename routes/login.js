59bce2cb8d15e35ea50027a82ee1bb2b60d85cea
Nicolas Marcelino <nicolas.marcelino.mota08@aluno.ifce.edu.br>
2023-05-04 11:32:49 -0300

    Mensagem de erro de autenticação na tela de login

--- routes/login.js
+++ routes/login.js
@@ -4,9 +4,9 @@ var router = express.Router()
 router.get('/', function (req, res, next) {
   var passedVariable = req.query.valid;
   if (passedVariable) {
-    res.render("login", { aviso: 'Senha incorreta.', estilo:'border: 2px solid #f44336;background-color: #ffebee;font-family: Arial, sans-serif;font-size: 14px;padding: 10px;border-radius: 4px;box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.15);' })
+    res.render("login", { aviso: 'Senha incorreta.', estilo:'border: 2px solid #f44336;background-color: #ffebee;font-family: Arial, sans-serif;font-size: 14px;padding: 10px;border-radius: 4px;box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.15);' });
   } else {
-    res.render("login", { aviso: '', estilo:'' })
+    res.render("login", { aviso: '', estilo:'' });
   }
 })
 
